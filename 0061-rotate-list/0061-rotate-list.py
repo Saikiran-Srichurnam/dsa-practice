c217aaa0f70d4a1d0d9dda4effdf742750b79a80
saikiran <srichurnamsaikiran628@gmail.com>
2026-08-07 18:58:37 +0530

    Time: 0 ms (100%), Space: 19.5 MB (9.85%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -5,34 +5,33 @@
 #         self.next = next
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        # base case 
         if not head or not head.next or k == 0:
             return head
-
+        
+        # find length of the linkedlist
         length = 1
         tail = head
         while tail.next:
             tail = tail.next
             length += 1
         
+        # Number of rotations
         k = k % length
 
+        # if k == 0 no rotations
         if k == 0:
             return head
-
-        # Make the linked list circular
+        
         tail.next = head
 
-        # k - length
-        newTail = head
+        # steps to reach newTail
         steps = length - k - 1
+        newTail = head
         for _ in range(steps):
             newTail = newTail.next
 
-        newHead = newTail.next 
+        newHead = newTail.next
         newTail.next = None
 
         return newHead
-
-
-
-        
