1253a52662664f69ecabeafc52d2167c354010f9
saikiran <srichurnamsaikiran628@gmail.com>
2026-08-13 08:10:08 +0530

    Time: 15 ms (40.57%), Space: 21.6 MB (97.6%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,11 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        grouped_anagrams = {}
-        for word in strs:
-            sorted_word = " ".join(sorted(word))
-            if sorted_word in grouped_anagrams:
-                grouped_anagrams[sorted_word].append(word)
-            else:
-                grouped_anagrams[sorted_word] = [word]
+        grp_anagrams = {}
 
-        return list(grouped_anagrams.values())
+        for word in strs:
+            sorted_wrd = "".join(sorted(word))
+            if sorted_wrd in grp_anagrams:
+                grp_anagrams[sorted_wrd].append(word)
+            else:
+                grp_anagrams[sorted_wrd] = [word]
+            
+        return list(grp_anagrams.values())
